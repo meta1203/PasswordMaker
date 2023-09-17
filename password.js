65835a819424178cc1370ad86f979af62e250140
Hunter Hancock <dr.miles.manners.ss@gmail.com>
2023-09-17 13:40:13 -0600

    BREAKING CHANGE: update generated password to always include at least one special character

--- password.js
+++ password.js
@@ -6,7 +6,12 @@ function generatePassword(key, site) {
 function domEdit() {
   var key = document.getElementById("key").value;
   var site = document.getElementById("site").value.toLowerCase();
-  var ret = generatePassword(key, site);
-  document.getElementById("output").value = ret.substring(0, 16);
+  var ret = generatePassword(key, site).substring(0, 16);
+  // ensure there is at least one special character in the
+  // generated password for improved site compatibility
+  if (!ret.includes("+") && !ret.includes("!"))
+    ret += "!";
+
+  document.getElementById("output").value = ret;
   document.getElementById("output").select();
 }
